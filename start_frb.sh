1ea15a83f6f4c4e18397822c65e54be911cd5359
Tammo Jan Dijkema <T.J.Dijkema@gmail.com>
2024-02-01 18:03:04 +0100

    Add sleep to give lband some time to start up

--- start_frb.sh
+++ start_frb.sh
@@ -4,4 +4,5 @@ screen -dmS filterbank_lband ./filterbank-lband.sh
 screen -dmS filterbank_l2band ./filterbank-l2band.sh
 screen -dmS filterbank_pband ./filterbank-pband.sh
 screen -dmS usrp_lband ./usrp-lband.sh
+sleep 1
 screen -dmS usrp_pband ./usrp-pband.sh
