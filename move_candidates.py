81a6bca654800acc957132ed773921fa81634684
Tammo Jan Dijkema <T.J.Dijkema@gmail.com>
2024-02-02 16:03:56 +0100

    Add move_candidates.py

---
+++ move_candidates.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+"""Read results from fetch, move files to subdirectories good and bad"""
+
+import shutil
+import os
+import pandas as pd
+
+df = pd.read_csv("results_a.csv")
+
+good = df[df['label'] == 1.0]
+bad  = df[df['label'] == 0.0]
+
+try:
+    os.mkdir("good")
+    os.mkdir("bad")
+except:
+    pass
+
+for fname in good['candidate']:
+    try:
+        shutil.move(fname, 'good')
+        shutil.move(fname.rstrip('.h5') + '.png', 'good')
+    except:
+        pass
+for fname in bad['candidate']:
+    try:
+        shutil.move(fname, 'bad')
+        shutil.move(fname.rstrip('.h5') + '.png', 'bad')
+    except:
+        pass
