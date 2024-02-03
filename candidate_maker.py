718683b10ecb603bb5d8450529f68d15fe96b602
Thomas Telkamp <thomas@telkamp.eu>
2024-02-03 12:14:09 +0100

    make PNG generation optional

--- candidate_maker.py
+++ candidate_maker.py
@@ -31,7 +31,7 @@ def make_output_name(filterbankname, cand):
     base = os.path.basename(filterbankname).rstrip(".fil")
     return f"{base}_tcand_{cand.tcand:.7f}_dm_{cand.dm:.1f}_snr_{cand.snr:.1f}.h5"
 
-def make_candidates_for_singlepulsefile(filterbankname, singlepulsename, sigma, time=None):
+def make_candidates_for_singlepulsefile(filterbankname, singlepulsename, sigma, time=None, image=False):
     try:
         df = pd.read_fwf(singlepulsename)
     except pd.errors.EmptyDataError:
@@ -45,14 +45,16 @@ def make_candidates_for_singlepulsefile(filterbankname, singlepulsename, sigma, 
         dm = df.iloc[0]["DM"]  # Assumes all DM are equal, which should be the case in a singlepulse file
         cand = make_candidate(filterbankname, dm, time, 10)  # Dummy sigma
         fout = cand.save_h5(fnout=make_output_name(filterbankname, cand))
-        plot_h5(fout, detrend_ft=True, save=True)
-        plt.close('all')
+        if (image):
+            plot_h5(fout, detrend_ft=True, save=True)
+            plt.close('all')
     else:
         for rownr, row in tqdm(df.iterrows(), total=len(df), leave=False):
             cand = make_candidate(filterbankname, row["DM"], row["Time (s)"], row["Sigma"])
             fout = cand.save_h5(fnout=make_output_name(filterbankname, cand))
-            plot_h5(fout, detrend_ft=True, save=True)
-            plt.close('all')
+            if (image):
+                plot_h5(fout, detrend_ft=True, save=True)
+                plt.close('all')
 
 
 def make_candidate(filterbankname, dm, tcand, sigma):
@@ -83,6 +85,7 @@ if __name__ == "__main__":
     parser.add_argument("-f", "--filterbankfile", help="Filterbank file (default: derived from singlepulse file, one directory up)")
     parser.add_argument("-s", "--sigma", help="Sigma (default 6.0)", default=6.0, type=float)
     parser.add_argument("-t", "--time", type=float, help="Do not look in the singlepulse file (just use its DM)")
+    parser.add_argument("-i", "--image", help="Create PNG files", action='store_true')
     parser.add_argument("-p", "--filterbankpath", help="Filterbank directory")
     args = parser.parse_args()
 
@@ -103,4 +106,4 @@ if __name__ == "__main__":
         else:
             filterbankfile = args.filterbankfile
     
-        make_candidates_for_singlepulsefile(filterbankfile, singlepulsefile, args.sigma, args.time)
+        make_candidates_for_singlepulsefile(filterbankfile, singlepulsefile, args.sigma, args.time, args.image)
