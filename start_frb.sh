94b318a73373db0a52700ce1d81b0022dd9c2b32
Tammo Jan Dijkema <T.J.Dijkema@gmail.com>
2024-02-04 12:00:00 +0100

    Hardcode path to ~/frb

--- start_frb.sh
+++ start_frb.sh
@@ -3,9 +3,9 @@ set -e  # Stop on first error
 
 ~/frb/get_frb_from_pointing.py  # Check if pointing close to FRB, stop otherwise
 
-screen -dmS filterbank_lband ./filterbank-lband.sh
-screen -dmS filterbank_l2band ./filterbank-l2band.sh
-screen -dmS filterbank_pband ./filterbank-pband.sh
-screen -dmS usrp_lband ./usrp-lband.sh
+screen -dmS filterbank_lband ~/frb/filterbank-lband.sh
+screen -dmS filterbank_l2band ~/frb/filterbank-l2band.sh
+screen -dmS filterbank_pband ~/frb/filterbank-pband.sh
+screen -dmS usrp_lband ~/frb/usrp-lband.sh
 sleep 1
-screen -dmS usrp_pband ./usrp-pband.sh
+screen -dmS usrp_pband ~/frb/usrp-pband.sh
