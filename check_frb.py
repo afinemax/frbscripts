0a4bfd13d4a3d79287940d852a66041814cf64fc
Tammo Jan Dijkema <T.J.Dijkema@gmail.com>
2024-02-04 18:06:36 +0100

    Fix check

--- check_frb.py
+++ check_frb.py
@@ -138,7 +138,7 @@ def main(relfilterbankfile, dm, dmrange, display, *, threshold=6, dry_run=False,
     if not dry_run:
         if not quiet:
             print(f"Going to create {num_pulse_candidates_exact_dm} candidates")
-        if num_pulse_candidates_exact_dm < 1000:
+        if num_pulse_candidates_exact_dm == "error" or num_pulse_candidates_exact_dm < 1000:
             make_candidates_for_singlepulsefile(filterbankfile, central_singlepulse_file)
         else:
             print(f"Not making {num_pulse_candidates_exact_dm} candidates for {filterbankname}")
