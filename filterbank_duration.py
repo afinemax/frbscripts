eebeb116338f978232c0204ebc2b1d7500e086f2
Tammo Jan Dijkema <T.J.Dijkema@gmail.com>
2024-02-11 13:22:25 +0100

    Prettify filterbank_duration

--- filterbank_duration.py
+++ filterbank_duration.py
@@ -3,14 +3,21 @@
 import sys
 from your.formats.pysigproc import SigprocFile
 from tqdm import tqdm
+from argparse import ArgumentParser
 
-total_duration_seconds = 0
+def main(file_list):
+    total_duration_seconds = 0
 
-for fname in tqdm(sys.argv[1:]):
-    fil = SigprocFile(fname)
-    total_duration_seconds += fil.native_tsamp() * fil.nspectra()
+    for fname in tqdm(sys.argv[1:]):
+        fil = SigprocFile(fname)
+        total_duration_seconds += fil.native_tsamp() * fil.nspectra()
 
-print("Seconds:", total_duration_seconds)
-print("Minutes:", total_duration_seconds / 60.)
-print("Hours  :", total_duration_seconds / 60. / 60.)
-print(f"Hours  : {total_duration_seconds // 3600:.0f}:{(total_duration_seconds % 3600) / 60.:02.0f}")
+    print(f"Hours  : {total_duration_seconds // 3600:.0f}:{(total_duration_seconds % 3600) / 60.:02.0f}")
+
+if __name__ == "__main__":
+    parser = ArgumentParser("Show duration of filterbank files")
+    parser.add_argument("files", nargs='+')
+
+    args = parser.parse_args()
+
+    main(args.files)
