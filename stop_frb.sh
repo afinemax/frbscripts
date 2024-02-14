3a6920bb68b61a3e7284a9ee06feb0f901224e82
Tammo Jan Dijkema <T.J.Dijkema@gmail.com>
2024-02-14 17:15:55 +0100

    Stop both 70cm-bands

--- stop_frb.sh
+++ stop_frb.sh
@@ -2,6 +2,7 @@
 #
 screen -XS usrp_pband quit
 screen -XS usrp_lband quit
-screen -XS filterbank_pband quit
+screen -XS filterbank_phband quit
+screen -XS filterbank_pvband quit
 screen -XS filterbank_l1band quit
 screen -XS filterbank_l2band quit
